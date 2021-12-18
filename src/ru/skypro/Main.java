f8d25cb2dbb652afc9a6149257aa0ca45b4eec8b
Саша <saniamit@mail.ru>
2021-12-18 16:41:05 +0300

    Дз по объектам и классам

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,45 +1,20 @@
 package ru.skypro;
 
-public class Book {
-    private final String authorName;
-    private final int pageAmount;
-    private String publisherName;
-    private int publishingYear;
+public class Main {
+   public static void main (String [] args) {
+       Author Tolstoy = new Author(" Lev ", " Tolstoy ");
+       Author Block = new Author(" Alexandr ", " Block ");
 
-    public Book(String authorName, int pageAmount, String publisherName, int publishingYear) {
-        this.authorName = authorName;
-        this.pageAmount = pageAmount;
-        this.publisherName = publisherName;
-        this.publishingYear = publishingYear;
-    }
+       Book warAndPiece = new Book(Tolstoy , " Война и мир ", 1869);
+       Book grayMorning = new Book(Block, " Седое утро ", 1920);
 
-    public String getAuthorName() {
-        return authorName;
-    }
+       System.out.println(warAndPiece);
+       System.out.println(grayMorning);
 
-    public int getPageAmount() {
-        return pageAmount;
-    }
-
-    public String getPublisherName() {
-        return publisherName;
-    }
-
-    public void setPublisherName(String publisherName) {
-        this.publisherName = publisherName;
-    }
-
-    public int getPublishingYear() {
-        return publishingYear;
-    }
-
-    public void setPublishingYear(int publishingYear) {
-        if (publishingYear < 1950 || publishingYear > 2050) {
-            System.out.println("Invalid publishing year parameter: " + publishingYear);
-            return;
-        }
-        this.publishingYear = publishingYear;
-    }
+       System.out.println(" Измение года выпуска книги ");
+       grayMorning.setPublisherYear(2020);
+       System.out.println(grayMorning);
+   }
 }
 
 
